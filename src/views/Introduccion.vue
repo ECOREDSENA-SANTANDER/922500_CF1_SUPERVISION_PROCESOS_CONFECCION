e31ada04aee28abac9d4b11f4e7b807b4216f5e6
jorozcop <maechavarriaor@gmail.com>
2023-12-14 09:32:20 -0500

    ajuste intro
    ajuste intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,20 @@
         span
           i.fas.fa-info
       h1 Introducción
-    
+
+    .row.justify-content-center.align-items-center.mb-5
+      p La industria textil-confeccionaria es un sector estratégico para la economía de muchos países, incluyendo a Colombia. Este sector genera empleo, impulsa el desarrollo tecnológico y contribuye a la satisfacción de las necesidades básicas de la población.
+
+      p En el contexto de la globalización, la competitividad de las empresas del sector textil-confeccionario depende, en gran medida, de su capacidad para optimizar sus procesos de producción. Esto implica, entre otras cosas, contar con un conocimiento profundo de los sistemas de producción y su aplicación en la cadena productiva de la confección industrial.
+
+      p El presente material didáctico tiene como objetivo conceptualizar sobre los sistemas de producción del proceso de confección de prendas de vestir. Para ello, se abordarán los siguientes aspectos: Terminología y conceptos básicos de los sistemas de producción, tipos de sistemas de producción, clasificación de los sistemas de producción, y los sistemas de gestión para garantizar la calidad en la producción.
+
+
+
+
+
+
+
 </template>
 
 <script>
