1bff6d1d9975d539276514f5d874bc0bfe1d8ccb
LizManchego <karinamanchego0416@gmail.com>
2024-05-31 09:07:13 -0600

    Fix extension pdf to zip

--- src/config/global.js
+++ src/config/global.js
@@ -219,7 +219,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF01_922500_DU.pdf',
+        download: 'downloads/CF01_922500_DU.zip',
       },
       {
         icono: 'fas fa-download',
